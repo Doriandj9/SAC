d0ecd802782f22c1a0825a651e5cfa0018f3f9a9
DorianA <dorian9armijos@gmail.com>
2021-09-28 10:45:57 -0500

    nuevo cambio

--- views/admin/editPermission.html.php
+++ views/admin/editPermission.html.php
@@ -2,9 +2,15 @@
 
 <div class="admin-permission">
     <form class="admin-permission-content">
+        <div class="fondo-permission">
+
             <?php foreach($permissions as $key=> $value): ?>
-                <input type="checkbox" name="permission[]">
+                <div>
+                <input type="checkbox" name="permission[]" value="<?= $value;?>">
                 <label for=""><?= $key ?></label>
+                </div>
             <?php endforeach; ?>
+            <input type="submit" value="Guardar" class="submit-permission"/>
+            </div>
     </form>
 </div>
