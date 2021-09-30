6eb7fc58438c80b8a669610a63c595054290e184
DorianA <dorian9armijos@gmail.com>
2021-09-29 22:34:35 -0500

    todo

--- public/js/main.js
+++ public/js/main.js
@@ -5,18 +5,18 @@ let correo_institucional = document.getElementById("email_institucional");
 
 correo_institucional.addEventListener("input",ingreso);
 
-function ingreso(){
-    const form = document.getElementById("form-login");
-    console.log(correo_institucional.value);
-    let expre = /[a-z0-9]/g;
-    if(!expre.test(correo_institucional.value)){
-        let div = document.createElement("div");
-        div.className ="div-error";
-        div.innerHTML = `<p> Error no es un correo insticuional </p>`;
-        form.append(div);
+// function ingreso(){
+//     const form = document.getElementById("form-login");
+//     console.log(correo_institucional.value);
+//     let expre = /[a-z0-9]/g;
+//     if(!expre.test(correo_institucional.value)){
+//         let div = document.createElement("div");
+//         div.className ="div-error";
+//         div.innerHTML = `<p> Error no es un correo insticuional </p>`;
+//         //form.append(div);
        
         
-    }
+//     }
 
     
-}
+// }
