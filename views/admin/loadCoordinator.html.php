450a8d5dd39323543abf1d24b24038445f8f71fa
Jerson-chimbo <jerson.chimbo2000@gmail.com>
2021-11-29 23:54:02 -0500

    vista terminada de ingreso de coordinadores

--- views/admin/loadCoordinator.html.php
+++ views/admin/loadCoordinator.html.php
@@ -2,48 +2,46 @@
     Ingresar Coordinadores:
 </div>
 <div class="tbing">
-                        <table>
-                            <form action="" method="post">
-                            <thead>
-                            <tr class="hdtb">
-                                <th>Cedula</th> 
-                                <th>Nombre y Apellido</th>
-                                <th>E-mail</th>
-                                <th>Carrera</th>
-                                
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr class="bytb">
-                                <td>
-                                    <input type="text" name="cedula">
-                                </td>
-                                <td>
-                                <input type="text" name="nombres">
-                                </td> 
-                                <td>
-                                <input type="email" name="email">
-                                </td>
-                                <td>
-                                <select name="carrera" id="anios">
+    <form action="" method="post">
+        <table class="table-coordinator">
+
+            <thead>
+                <tr class="hdtb">
+                    <th>Cedula</th>
+                    <th>Nombre y Apellido</th>
+                    <th>E-mail</th>
+                    <th>Carrera</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <input type="text" name="cedula" required>
+                    </td>
+                    <td>
+                        <input type="text" name="nombres" required>
+                    </td>
+                    <td>
+                        <input type="email" name="email" required>
+                    </td>
+                    <td>
+                        <select name="carrera" id="anios" required>
                             <option value="#" selected disabled>-- Asignar Carrera --</option>
                             <option value="">Software</option>
                             <option value="">Turismo</option>
-                            
-                        </select> 
-                                </td>
-                            </tr>
-                            </tbody>
-                            </form>
-                        </table>
-                        
-                    </div>
 
-                    <div id="ba">  
-                     <div class="content-for-evaluation">
-                     <input type="submit" value="Guardar" id="btnGuardar">
-                    </div>
-                    </div>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div id="ba">
+            <div class="content-for-evaluation">
+                <input type="submit" value="Guardar" id="btnGuardar">
+            </div>
+        </div>
+    </form>
 
 
-                    
+</div>
