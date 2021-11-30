9f9088501763810a1369b82100ddf5da3feeb66d
Jerson-chimbo <jerson.chimbo2000@gmail.com>
2021-11-29 22:59:51 -0500

    coordinador

--- views/admin/loadCoordinator.html.php
+++ views/admin/loadCoordinator.html.php
@@ -1,1 +1,43 @@
-<h2>Vista de ingresar coordinadores</h2>
+<div id="cnt" class="ideb">
+    Ingresar Coordinadores:
+</div>
+<div class="tbing">
+                        <table>
+                            <form action="" method="post">
+                            <thead>
+                            <tr class="hdtb">
+                                <th>Cedula</th> 
+                                <th>Nombre y Apellido</th>
+                                <th>E-mail</th>
+                                <th>Carrera</th>
+                                
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr class="bytb">
+                                <td>
+                                    <input type="text" name="cedula">
+                                </td>
+                                <td>
+                                <input type="text" name="nombres">
+                                </td> 
+                                <td>
+                                <input type="email" name="email">
+                                </td>
+                                <td>
+                                <select name="carrera" id="anios">
+                            <option value="#" selected disabled>-- seleccione una opción --</option>
+                            <option value="">Software</option>
+                            <option value="">Turismo</option>
+                            
+                        </select> 
+                                </td>
+                            </tr>
+                            </tbody>
+                            </form>
+                        </table>
+                        
+                    </div>
+
+
+                    
