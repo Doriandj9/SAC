d9a1656a9e96290d301a3db6b02579cc0677134e
DorianA <dorian9armijos@gmail.com>
2021-12-01 10:44:14 -0500

    cambios

--- src/web/Utiles.php
+++ src/web/Utiles.php
@@ -5,7 +5,7 @@ namespace web;
 class Utiles{
 
 
-    public function hashPassword($string): string
+    public static function  hashPassword($string): string
     {
         $password = password_hash($string, PASSWORD_DEFAULT);
 
